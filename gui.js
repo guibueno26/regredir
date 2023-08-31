6d3a5b00edf448b562f00070c6f4d65eb746daea
guibueno26 <124888546+guibueno26@users.noreply.github.com>
2023-08-31 09:17:02 -0300

    Add files via upload

---
+++ gui.js
@@ -0,0 +1,11 @@
+tempo = 5000;
+const idade = 17;
+age = 17;
+age = age + 1;
+var timer =setInterval(chamar, tempo);
+
+function chamar(){
+ //document.getElement só é chamado com tempo
+document.getElementById("mostrar").innerHTML = idade;
+document.getElementById("exibir").innerHTML = age;
+}
